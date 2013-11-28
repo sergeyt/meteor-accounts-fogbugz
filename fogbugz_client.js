fbcce4739a8a95baeebb0696e56158850b4d4392
sergeyt <stodyshev@gmail.com>
2013-11-28 22:34:39 +0700

    reformat

--- fogbugz_client.js
+++ fogbugz_client.js
@@ -1,20 +1,20 @@
-(function () {
-  Meteor.loginWithFogBugz = function (options, callback) {
-    // support both (options, callback) and (callback).
-    if (!callback && typeof options === 'function') {
-      callback = options;
-      options = {};
-    }
+(function() {
+	Meteor.loginWithFogBugz = function(options, callback) {
+		// support both (options, callback) and (callback).
+		if (!callback && typeof options === 'function') {
+			callback = options;
+			options = {};
+		}
 
-    /**
-     * login callback to call user callback
-     * @param error
-     */
-    var loginCallback = function (error) {
-      if (!error && callback) {
-        callback();
-      }
-    };
+		/**
+		 * login callback to call user callback
+		 * @param error
+		 */
+		var loginCallback = function(error) {
+			if (!error && callback) {
+				callback();
+			}
+		};
 
 		Accounts.callLoginMethod({
 			methodName: 'login',
@@ -27,5 +27,5 @@
 			],
 			userCallback: loginCallback
 		});
-  };
+	};
 })();
