5d962cff0a7fe0aff202ecdbb3fbf15a00cac1d6
sergeyt <stodyshev@gmail.com>
2013-11-30 11:57:34 +0700

    adding log for dev needs

--- fogbugz_server.js
+++ fogbugz_server.js
@@ -13,15 +13,25 @@
 		if (!options.fogbugz && !options.email && !options.password)
 			return undefined; // don't handle
 
-		if (options.fogbugz.charAt(options.fogbugz.length - 1) != '/'){
-			options.fogbugz += '/';
+		var verbose = options.verbose;
+		function log(msg){
+			if (verbose) {
+				console.log('fogbugz: ' + msg);
+			}
 		}
 
-		var url = options.fogbugz + 'api.asp?cmd=logon' +
+		var endpoint = options.fogbugz;
+		if (endpoint.charAt(endpoint.length - 1) != '/'){
+			endpoint += '/';
+		}
+
+		var url = endpoint + 'api.asp?cmd=logon' +
 				'&email=' + encodeURIComponent(options.email) +
 				'&password=' + encodeURIComponent(options.password);
 
 		var xml = Meteor.http.get(url);
+		log(xml);
+
 		var match = (/<token>(.*)<\/token>/g).exec(xml);
 		if (match){
 			var token = match[1];
